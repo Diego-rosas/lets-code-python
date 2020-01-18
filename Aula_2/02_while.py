025cd5790ce08f3dc2b5348e4b7ae2ae4c0f99f6
Vladimir Lemos <vladimirlemos@yahoo.com.br>
2020-01-18 12:03:09 -0300

    estudar bem o while

--- Aula_2/02_while.py
+++ Aula_2/02_while.py
@@ -3,4 +3,12 @@
 
 lista_qualquer = [1,2,3,4,5,6,7,8,9,10]
 
-while 
+controle = len(lista_qualquer)
+
+while controle > 0:
+    print(lista_qualquer[controle -1])
+    controle = controle - 1
+
+
+
+    
