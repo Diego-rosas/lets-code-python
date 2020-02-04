fdf169106873b1db4eccd03af6c7717655bd483f
Vladimir Lemos <vladimirlemos@yahoo.com.br>
2020-02-04 04:11:23 -0300

    pensando no projeto

---
+++ projeto/caixa_imoveis.py
@@ -0,0 +1,14 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+def make_soup(url):
+    thepage = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(thepage, "html.parser")
+    return soupdata
+
+soup = make_soup("https://venda-imoveis.caixa.gov.br/listaweb/Lista_imoveis_SP.htm?43193352")
+
+for registros in soup.findAll("tr"):
+    for dados in registros.findAll("td"):
+        print(dados.text)
