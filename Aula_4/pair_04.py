8f23698c051d0cbcf5511064d88196dbb339b4ea
Vladimir Lemos <vladimirlemos@yahoo.com.br>
2020-02-08 20:17:07 -0300

    ainda não está bom

--- Aula_4/pair_04.py
+++ Aula_4/pair_04.py
@@ -3,21 +3,30 @@
  e a quantidade de aparições
  '''
 
+def conta_letras_frase(frase):
+    letras = []
+    for palavra in frase:
+        if palavra not in letras:
+            letras.append(palavra)
+
+    # letras.remove(' ')
+
+    lista_final = []
+    
+    for letra in letras:
+        x = 0
+        for repeticoes in frase:
+            if repeticoes == letra:
+                x = x + 1
+        
+        if letra != ' ':
+            print (letra, x)
+            tupla = (letra , x) # aqui eu poderia só dar um print, mas vou seguir com tuplas;
+        lista_final.append(tupla)
+
+    
+    return lista_final
+
 frase = input('digite algo: ')
 
-letras = []
-for palavra in frase:
-    if palavra not in letras:
-        letras.append(palavra)
-
-# letras.remove(' ')
-
-lista_final = []
-
-for letra in letras:
-    x = 0
-    for repeticoes in frase:
-        if repeticoes == letra:
-            x = x + 1
-    print('A letra:', letra ,'aparece', x , 'vezes')
-
+print('resultado da expressão', frase, ':', conta_letras_frase(frase))
